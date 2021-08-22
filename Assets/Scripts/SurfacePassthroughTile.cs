f052721b04bf821b000e4e9577d005e89620d1a9
Dilmer Valecillos <dilmervst@gmail.com>
2021-08-22 11:17:59 -0600

    Added debugger window, surface tiles, and control input actions

---
+++ Assets/Scripts/SurfacePassthroughTile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SurfacePassthroughTile : MonoBehaviour
+{
+    private MeshFilter projectionMeshFilter;
+
+    private MeshRenderer outlineMeshRenderer;
+
+    private const int ACTIVATE_TILE_NUM = 1;
+
+    void Start()
+    {
+        projectionMeshFilter = GetComponentInChildren<MeshFilter>();
+        outlineMeshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
+    public void RandomToggle()
+    {
+        if(Random.Range(0,1) == ACTIVATE_TILE_NUM)
+        {
+            SurfaceProjectionManager.Instance.PassthroughLayer
+            .AddSurfaceGeometry(projectionMeshFilter.gameObject, true);
+
+            outlineMeshRenderer.enabled = false;
+        }
+        else
+        {
+            SurfaceProjectionManager.Instance.PassthroughLayer
+           .RemoveSurfaceGeometry(projectionMeshFilter.gameObject);
+
+            outlineMeshRenderer.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SurfaceProjectionManager.cs
@@ -0,0 +1,55 @@
+﻿using DilmerGames.Core.Singletons;
+using UnityEngine;
+
+public class SurfaceProjectionManager : Singleton<SurfaceProjectionManager>
+{
+    [SerializeField]
+    private OVRPassthroughLayer passthroughLayer;
+
+    [SerializeField]
+    private Transform tilesRoot;
+
+    public OVRPassthroughLayer PassthroughLayer => passthroughLayer;
+
+    public Transform TilesRoot => tilesRoot;
+
+    // default is user defined
+    private bool reconstructedPassthrough = true;
+
+    public void ShuffleTiles()
+    {
+        var tiles = tilesRoot.GetComponentsInChildren<SurfacePassthroughTile>();
+
+        foreach (var tile in tiles)
+        {
+            tile.RandomToggle();
+        }
+    }
+
+    public void Update()
+    {
+        // A button
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Logger.Instance.LogInfo("Shuffle tiles executed");
+            ShuffleTiles();
+        }
+
+        // B button
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            reconstructedPassthrough = !reconstructedPassthrough;
+
+            if(reconstructedPassthrough)
+            {
+                passthroughLayer.projectionSurfaceType = OVRPassthroughLayer.ProjectionSurfaceType.Reconstructed;
+                Logger.Instance.LogInfo("Toggle passthrough set to reconstructed");
+            }
+            else
+            {
+                passthroughLayer.projectionSurfaceType = OVRPassthroughLayer.ProjectionSurfaceType.UserDefined;
+                Logger.Instance.LogInfo("Toggle passthrough set to user defined");
+            }
+        }
+    }
+}
